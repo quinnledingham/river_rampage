62443b257db7433f104c27d537f472f1eb15169c
quinnledingham <quinnledingham@gmail.com>
2023-04-23 16:41:24 -0600

    bat file

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+pushd %CD%
+IF NOT EXIST build mkdir build
+
+set CFs= -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 /Isdl-vc\include /Iglad /Istb /Iqlib /Fobuild/river
+set LFs= -incremental:no -opt:ref shell32.lib opengl32.lib sdl-vc\lib\x64\SDL2main.lib sdl-vc\lib\x64\SDL2.lib /subsystem:console
+
+cl %CFs% game.cpp /link %LFs% /out:build\river.exe
+
+IF NOT EXIST build\SDL2.dll copy sdl-vc\lib\x64\SDL2.dll build
