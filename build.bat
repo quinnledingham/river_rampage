12e1753ce791088320ab9769962285e324f15004
Quinn <quinnledingham@gmail.com>
2023-10-30 16:15:37 -0600

    remove files

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 pushd %CD%
 IF NOT EXIST build mkdir build
 
-set CFs= -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 /Isdl-vc\include /Iglad /Istb /Iqlib  /DWINDOWS
+set CFs= -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 /Isdl-vc\include /Iglad /Istb /Iqlib /DWINDOWS
 set LFs= -incremental:no -opt:ref shell32.lib opengl32.lib sdl-vc\lib\x64\SDL2main.lib sdl-vc\lib\x64\SDL2.lib /subsystem:console
 
 cl %CFs% game.cpp application.cpp /link %LFs% /out:build\river.exe
