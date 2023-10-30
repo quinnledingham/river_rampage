94d76473d815c60968a69bedb9952f075e4ea26b
Quinn <quinnledingham@gmail.com>
2023-10-30 16:20:04 -0600

    adjusting build.bat to put obj files in build folder

--- build.bat
+++ build.bat
@@ -2,10 +2,11 @@
 
 pushd %CD%
 IF NOT EXIST build mkdir build
+cd build
 
-set CFs= -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 /Isdl-vc\include /Iglad /Istb /Iqlib /DWINDOWS
-set LFs= -incremental:no -opt:ref shell32.lib opengl32.lib sdl-vc\lib\x64\SDL2main.lib sdl-vc\lib\x64\SDL2.lib /subsystem:console
+set CFs= -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 /I..\sdl-vc\include /I..\glad /I..\stb /DWINDOWS
+set LFs= -incremental:no -opt:ref shell32.lib opengl32.lib ..\sdl-vc\lib\x64\SDL2main.lib ..\sdl-vc\lib\x64\SDL2.lib /subsystem:console
 
-cl %CFs% game.cpp application.cpp /link %LFs% /out:build\river.exe
+cl %CFs% ..\game.cpp ..\application.cpp /link %LFs% /out:river.exe
 
-IF NOT EXIST build\SDL2.dll copy sdl-vc\lib\x64\SDL2.dll build
+IF NOT EXIST SDL2.dll copy ..\sdl-vc\lib\x64\SDL2.dll build
